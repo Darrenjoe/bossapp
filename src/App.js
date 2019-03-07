0f0d6438f114ba9940e340aea79354a0e31aa0ca
Darren <491361994@qq.com>
2019-03-07 23:21:29 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// eslint-disable-next-line
-import {createStore} from 'redux';
+import React from 'react'
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Darren</h1>
+        <Age></Age>
       </div>
-    );
+    )
+  }
+}
+class Age extends React.Component {
+  render() {
+    const age = '24'
+    return <h2>{age}</h2>
   }
 }
 
-export default App;
+export default App
