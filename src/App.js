338880a6f7e0ffd320fd55beef18bd3d50fa0516
Darren <491361994@qq.com>
2019-03-09 22:32:05 +0800

    add antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Button} from 'antd-mobile'
 
 class App extends React.Component {
   render() {
@@ -23,10 +24,17 @@ class Age extends React.Component {
       ages: [13, 24, 31, 16,]
     }
   }
+  addAge() {
+    console.log('asd');
+    this.setState({
+      ages: [...this.state.ages, '18']
+    })
+  }
   render() {
     return (
       <div>
         <h2>{this.props.age}</h2>
+        <Button type="primary" onClick={this.addAge}>Add Age</Button>
         <ul>
           {this.state.ages.map(v => {
             return <li key={v}>{v}</li>
