4674a6b36754cca26c24fe3ca711e9e94ceb64cb
Darren <491361994@qq.com>
2019-06-08 16:55:47 +0800

    demo

--- demo/src/Food.js
+++ demo/src/Food.js
@@ -1,18 +1,56 @@
 import React, { Component, Fragment } from 'react'
+import './index.css'
 
 class Food extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: 'Darren',
+      list: ['干锅肥肠', '佛跳墙']
+    }
+  }
   render() {
     return (
       <Fragment>
         <div>
-          <input /> <button>增加菜单</button>
+          <label htmlFor="editor">增加菜品:</label>
+          <input id="editor" className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
+          <button onClick={this.addList.bind(this)}>增加菜单</button>
         </div>
         <ul>
-          <li>红烧肉</li>
-          <li>东坡肘子</li>
+          {
+            this.state.list.map((item, index) => {
+              return <li 
+                        key={index + item}
+                        onClick={this.deleteItem.bind(this, index)}
+                        dangerouslySetInnerHTML={{__html: item}}
+                      ></li>
+            })
+          }
         </ul>
       </Fragment>
     )
+  }
+
+  inputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  addList() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  deleteItem(index) {
+    let list = this.state.list
+    list.splice(index,1);
+    this.setState({
+      list: list
+    })
   }
 }
 
