82b96c04d6d77bc6e915c634b7636f2b016122ba
Darren <491361994@qq.com>
2019-06-30 23:09:14 +0800

    redux

--- demo/src/TodoList.js
+++ demo/src/TodoList.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Input, Button, List } from "antd";
+import "antd/dist/antd.css";
+
+const data = ["代码编写", "看看书", "看看报纸"];
 
 class TodoList extends Component {
-  
-  render() { 
+  render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <div style={{ margin: "10px" }}>
+          <Input
+            placeholder="write Something"
+            style={{ width: "250px", marginRight: "10px" }}
+          />
+          <Button type="primary">Add</Button>
+        </div>
+        <div style={{ margin: "10px", width: "300px" }}>
+          <List
+            bordered
+            dataSource={data}
+            renderItem={item => <List.Item>{item}</List.Item>}
+          />
+        </div>
+      </div>
     );
   }
 }
- 
+
 export default TodoList;
