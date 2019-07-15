2a595bc07c4540c0d43289c82b46acb2a89d09b5
Darrenjoe <491361994@qq.com>
2019-07-15 23:01:05 +0800

    拆分组件

--- demo/src/TodoList.js
+++ demo/src/TodoList.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Input, Button, List } from "antd";
 import "antd/dist/antd.css";
 import store from "./store";
 import {
@@ -7,6 +6,7 @@ import {
   addItemAction,
   deleteItemAciton
 } from "./store/actionCreators";
+import TodoListUI from "./TodoListUI";
 
 class TodoList extends Component {
   constructor(props) {
@@ -15,34 +15,18 @@ class TodoList extends Component {
     this.changeInputValue = this.changeInputValue.bind(this);
     this.clickBtn = this.clickBtn.bind(this);
     this.storeChange = this.storeChange.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
     store.subscribe(this.storeChange);
   }
   render() {
     return (
-      <div>
-        <div style={{ margin: "10px" }}>
-          <Input
-            placeholder={this.state.inputValue}
-            style={{ width: "250px", marginRight: "10px" }}
-            value={this.state.inputValue}
-            onChange={this.changeInputValue}
-          />
-          <Button type="primary" onClick={this.clickBtn}>
-            Add
-          </Button>
-        </div>
-        <div style={{ margin: "10px", width: "300px" }}>
-          <List
-            bordered
-            dataSource={this.state.list}
-            renderItem={(item, index) => (
-              <List.Item onClick={this.deleteItem.bind(this, index)}>
-                {item}
-              </List.Item>
-            )}
-          />
-        </div>
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        changeInputValue={this.changeInputValue}
+        clickBtn={this.clickBtn}
+        list={this.state.list}
+        deleteItem={this.deleteItem}
+      />
     );
   }
 
@@ -56,11 +40,13 @@ class TodoList extends Component {
   }
 
   clickBtn() {
+    if (!this.state.inputValue) return;
     const action = addItemAction();
     store.dispatch(action);
   }
 
   deleteItem(index) {
+    console.log(index);
     const action = deleteItemAciton(index);
     store.dispatch(action);
   }
