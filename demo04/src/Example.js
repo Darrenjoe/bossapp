f20d4666bccc946e97a1d524eceb2792cc85cf1a
Darrenjoe <491361994@qq.com>
2019-08-31 12:06:54 +0800

    useEffect

--- demo04/src/Example.js
+++ demo04/src/Example.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  useEffect(() => {
+    console.log(`useEffect=>come to Index`);
+    return () => {
+      console.log("go out Index");
+    };
+  }, []);
+  return <h2>Darren</h2>;
+}
+function List() {
+  return <h2>List</h2>;
+}
+
 function Example() {
   const [count, setCount] = useState(0);
   useEffect(() => {
     console.log(`useEffect=>You Click ${count} times`);
-  });
+    return () => {
+      console.log("========================");
+    };
+  }, [count]);
   return (
     <div>
       <p>You clicked {count} times</p>
@@ -14,6 +32,18 @@ function Example() {
       >
         Click me
       </button>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">首页</Link>
+          </li>
+          <li>
+            <Link to="/list/">列表</Link>
+          </li>
+        </ul>
+        <Route path="/" exact component={Index} />
+        <Route path="/list" exact component={List} />
+      </Router>
     </div>
   );
 }
