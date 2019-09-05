629833eea0a03d218dbe2946c32387f3a3bb89a7
Darrenjoe <491361994@qq.com>
2019-09-05 20:50:34 +0800

    useCallback

--- demo04/src/Example8.js
+++ demo04/src/Example8.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function Example8() {
   const inputEl = useRef(null);
@@ -7,10 +7,27 @@ function Example8() {
     console.log(inputEl);
   };
 
+  const [text, setText] = useState("Darren");
+  const textRef = useRef();
+
+  useEffect(() => {
+    textRef.current = text;
+    console.log("text", textRef.current);
+  });
+
   return (
     <div>
       <input ref={inputEl} type="text" />
       <button onClick={onButtonClick}>show input on text</button>
+      <br></br>
+      <br></br>
+      <input
+        value={text}
+        onChange={e => {
+          setText(e.target.value);
+        }}
+      />
+      <h2>{text}</h2>
     </div>
   );
 }
