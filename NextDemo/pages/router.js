70b0b51ce954e318e695377c90b6d7734b1b206f
Darrenjoe <491361994@qq.com>
2019-09-07 11:50:29 +0800

    next/router

--- NextDemo/pages/router.js
+++ NextDemo/pages/router.js
@@ -2,29 +2,28 @@ import React from "react";
 import Link from "next/link";
 import Router from "next/router";
 
-const Home = () => (
-  <div>
-    <h2>Router</h2>
+const Home = () => {
+  function gotoA() {
+    Router.push("/DarrenA");
+  }
+  return (
     <div>
-      <Link href="/DarrenA">
-        <a>goto DarrenA</a>
-      </Link>
+      <h2>Router</h2>
+      <div>
+        <Link href="/DarrenA">
+          <a>goto DarrenA</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/DarrenB">
+          <a>goto DarrenB</a>
+        </Link>
+      </div>
+      <div>
+        <button onClick={gotoA}>goto A</button>
+      </div>
     </div>
-    <div>
-      <Link href="/DarrenB">
-        <a>goto DarrenB</a>
-      </Link>
-    </div>
-    <div>
-      <button
-        onClick={() => {
-          Router.push("/DarrenA");
-        }}
-      >
-        goto A
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
