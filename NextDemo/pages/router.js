3e0335bc37d05b8f8db016c130dc94025d7d4bec
Darrenjoe <491361994@qq.com>
2019-09-16 21:05:13 +0800

    router

--- NextDemo/pages/router.js
+++ NextDemo/pages/router.js
@@ -3,6 +3,19 @@ import Link from "next/link";
 import Router from "next/router";
 
 const Home = () => {
+  //routeChangeStart
+  //routeChangeComplete
+  //beforeHistoryChange
+  //routeCHangeError
+  //hashChangeStart
+  //hashChangeComplete
+
+  Router.events.on("routeChange Start", (...args) => {
+    console.log("routeChangeStart->路由开始变化", ...args);
+  });
+  Router.events.on("routeChangeComplete", (...args) => {
+    console.log("routeChangeComplete->路由变化结束", ...args);
+  });
   return (
     <div>
       <h2>Router</h2>
@@ -13,6 +26,11 @@ const Home = () => {
         <br />
         <Link href="/jiewu?name=locking">
           <a>选择Locking</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="#Darren">
+          <a>Darren</a>
         </Link>
       </div>
     </div>
