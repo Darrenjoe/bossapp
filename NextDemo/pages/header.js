bf0e6113fa1c72b2c5bcad73628bab876c8a4070
Darrenjoe <491361994@qq.com>
2019-09-22 11:37:01 +0800

    import css

--- NextDemo/pages/header.js
+++ NextDemo/pages/header.js
@@ -1,10 +1,13 @@
 import MyHeader from "../components/myheader";
-
+import { Button } from "antd";
 function Header() {
   return (
     <>
       <MyHeader />
       <div>Darren.com</div>
+      <div>
+        <Button>按钮</Button>
+      </div>
     </>
   );
 }
