b22ef6951f2485a2a109bbc6a43ae4f57d6443d2
Darrenjoe <491361994@qq.com>
2019-09-23 22:17:31 +0800

    blog

---
+++ react_blog/blog/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import { Button } from "antd";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div>
+      <Button>按钮</Button>
+    </div>
+  </div>
+);
+
+export default Home;
