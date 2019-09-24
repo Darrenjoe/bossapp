35db974aa724b34d9cc5063edb1fc879be2e1616
Darrenjoe <491361994@qq.com>
2019-09-24 19:47:29 +0800

    header

--- react_blog/blog/pages/index.js
+++ react_blog/blog/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Head from "next/head";
-import { Button } from "antd";
+import Header from "../components/Header";
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
     </Head>
-    <div>
-      <Button>按钮</Button>
-    </div>
+    <Header />
   </div>
 );
 
