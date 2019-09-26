1204b31a65ed5e7afdb8b1799348f10082e88328
Darrenjoe <491361994@qq.com>
2019-09-26 23:24:37 +0800

    comm

--- react_blog/blog/pages/index.js
+++ react_blog/blog/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Header from "../components/Header";
+import { Row, Col, Button } from "antd";
 
 const Home = () => (
   <div>
@@ -8,6 +9,14 @@ const Home = () => (
       <title>Home</title>
     </Head>
     <Header />
+    <Row className="comm-main" type="flex" justify="center">
+      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+        左侧
+      </Col>
+      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+        右侧
+      </Col>
+    </Row>
   </div>
 );
 
