fe54e1dca1feb37b8076c76568d3d5e307550b7f
Darrenjoe <491361994@qq.com>
2019-10-21 22:07:33 +0800

    连接数据库

--- react_blog/service/config/config.default.js
+++ react_blog/service/config/config.default.js
@@ -10,7 +10,7 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1570716241704_7900';
@@ -21,6 +21,26 @@ module.exports = appInfo => {
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
+  };
+
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: 'root',
+      // database
+      database: 'react_blog',
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
   };
 
   return {
