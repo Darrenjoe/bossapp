a9327078ba30124b2e3fa9c623e2ab50a41e8385
Darrenjoe <491361994@qq.com>
2019-10-25 22:52:56 +0800

    marked

--- react_blog/blog/pages/detailed.js
+++ react_blog/blog/pages/detailed.js
@@ -6,47 +6,31 @@ import Author from "../components/Author";
 import Advert from "../components/Advert";
 import Footer from "../components/Footer";
 import "../static/style/pages/detailed.css";
-import ReactMarkdown from "react-markdown";
 import MarkNav from "markdown-navbar";
 import "markdown-navbar/dist/navbar.css";
 import axios from "axios";
 
-const Detailed = () => {
-  let markdown =
-    "# P01:课程介绍和环境搭建\n" +
-    "[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n" +
-    "> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n" +
-    "**这是加粗的文字**\n\n" +
-    "*这是倾斜的文字*`\n\n" +
-    "***这是斜体加粗的文字***\n\n" +
-    "~~这是加删除线的文字~~ \n\n" +
-    "`console.log(111)` \n\n" +
-    "# p02:来个Hello World 初始Vue3.0\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n" +
-    "***\n\n\n" +
-    "# p03:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p04:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "#5 p05:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p06:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p07:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "``` var a=11; ```";
+import marked from "marked";
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
+
+const Detailed = props => {
+  const renderer = new marked.Renderer();
+
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
+
+  let html = marked(props.article_content);
 
   return (
     <div>
@@ -84,9 +68,10 @@ const Detailed = () => {
                   200
                 </span>
               </div>
-              <div className="detailed-content">
-                <ReactMarkdown source={markdown} escapeHtml={false} />
-              </div>
+              <div
+                className="detailed-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              ></div>
             </div>
           </div>
         </Col>
@@ -96,7 +81,7 @@ const Detailed = () => {
           <Affix offsetTop={5}>
             <div className="detailed-nav comm-box">
               <div className="nav">文章目录</div>
-              <MarkNav className="article" source={markdown} ordered={false} />
+              <MarkNav className="article" source={html} ordered={false} />
             </div>
           </Affix>
         </Col>
