79f3fa801ce914d921cf939cb9b185c51d389ef8
Darrenjoe <491361994@qq.com>
2019-11-05 22:56:43 +0800

    getList

--- react_blog/service/app/router/default.js
+++ react_blog/service/app/router/default.js
@@ -1,12 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/default/index', controller.default.home.index);
-  router.get('/default/getArticleList', controller.default.home.getArticleList);
+  router.get("/default/index", controller.default.home.index);
+  router.get("/default/getArticleList", controller.default.home.getArticleList);
   router.get(
-    '/default/getArticleById/:id',
+    "/default/getArticleById/:id",
     controller.default.home.getArticleById
   );
-  router.get('/default/getTypeInfo', controller.default.home.getTypeInfo);
+  router.get("/default/getTypeInfo", controller.default.home.getTypeInfo);
+  router.get("/default/getListById/:id", controller.default.home.getListById);
 };
