6e94613a14ce7a2db4b9022f358d1f95197aba5d
Darrenjoe <491361994@qq.com>
2019-12-07 21:20:44 +0800

    login

--- react_blog/admin/src/Pages/Login.js
+++ react_blog/admin/src/Pages/Login.js
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
 import "antd/dist/antd.css";
-import { Card, Input, Icon, Button, Spin } from "antd";
+import { Card, Input, Icon, Button, Spin, message } from "antd";
 import "../static/css/Login.css";
+import axios from "axios";
+import servicePath from "../config/apiUrl";
 
-function Login() {
+function Login(props) {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const checkLogin = () => {
     setIsLoading(true);
+    if (!userName) {
+      message.error("用户名不能为空");
+      return false;
+    }
+    if (!password) {
+      message.error("密码不能为空");
+      return false;
+    }
+    let dataProps = {
+      userName: userName,
+      password: password
+    };
+    axios({
+      method: "post",
+      url: servicePath.checkLogin,
+      data: dataProps,
+      withCredentials: true
+    }).then(res => {
+      setIsLoading(false);
+      if (res.data.data == "登录成功") {
+        localStorage.setItem("openId", res.data.openId);
+        props.history.push("/index");
+      } else {
+        message.error("用户名或密码错误");
+      }
+    });
     setTimeout(() => {
       setIsLoading(false);
     }, 1000);
@@ -23,15 +51,10 @@ function Login() {
             id="userName"
             size="large"
             placeholder="Enter your userName"
-            prefix={
-              <Icon
-                type="user"
-                style={{ color: "rgba(0,0,.25)" }}
-                onChange={e => {
-                  setUserName(e.target.value);
-                }}
-              />
-            }
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,.25)" }} />}
+            onChange={e => {
+              setUserName(e.target.value);
+            }}
           />
           <br />
           <br />
@@ -39,15 +62,10 @@ function Login() {
             id="password"
             size="large"
             placeholder="Enter your password"
-            prefix={
-              <Icon
-                type="key"
-                style={{ color: "rgba(0,0,.25)" }}
-                onChange={e => {
-                  setPassword(e.target.value);
-                }}
-              />
-            }
+            prefix={<Icon type="key" style={{ color: "rgba(0,0,.25)" }} />}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
           />
           <br />
           <br />
