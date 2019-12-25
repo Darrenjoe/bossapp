642080ffb8c9b209e9552f4c4f90d7b25665f8b5
Darrenjoe <491361994@qq.com>
2019-12-25 20:09:52 +0800

    sevice

--- react_blog/service/app/middleware/adminauth.js
+++ react_blog/service/app/middleware/adminauth.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-module.exports = Option => {
+module.exports = options => {
   return async function adminauth(ctx, next) {
     console.log(ctx.session.openId);
     if (ctx.session.openId) {
       await next();
     } else {
       ctx.body = {
-        data: '没有登录',
+        data: "没有登录"
       };
     }
   };
